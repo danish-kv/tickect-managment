75e4d673fb61560b2eaa7e9a7df0878e31e91b6e
Danish <muhammeddanishkv05@gmail.com>
2024-12-23 23:05:07 +0530

    feat: added index for model fields, meta class for ordering

--- backend/ticket/models.py
+++ backend/ticket/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, db_index=True)
     profile = models.ImageField(upload_to='profiles/', null=True, blank=True)
     number = models.CharField(max_length=15, null=True, blank=True) 
     otp = models.CharField(max_length=6, null=True, blank=True)
@@ -13,7 +13,9 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
-
+    
+    class Meta:
+        ordering = ['-id']
 
 class Ticket(models.Model):
     PRIORITY_STATUS = [
@@ -27,20 +29,23 @@ class Ticket(models.Model):
         ('Resolved', 'Resolved'),
     ]
 
-    title = models.CharField(max_length=250, null=True, blank=True)
+    title = models.CharField(max_length=250, null=True, blank=True,db_index=True)
     description = models.TextField(null=True, blank=True)
     image = models.FileField(upload_to='uploads/tickets/', null=True, blank=True)  
-    priority = models.CharField(max_length=10, choices=PRIORITY_STATUS, default='Low')
-    status = models.CharField(max_length=15, choices=CURRENT_STATUS, default='Open')
-    created_at = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=10, choices=PRIORITY_STATUS, default='Low',db_index=True)
+    status = models.CharField(max_length=15, choices=CURRENT_STATUS, default='Open', db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tickets_created', null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tickets_created', null=True, db_index=True)
     assigned_to = models.ForeignKey(
-        CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='tickets_assigned'
+        CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='tickets_assigned', db_index=True
     )
 
     def __str__(self):
         return f"Ticket: {self.title} ({self.status})"
+    
+    class Meta:
+        ordering = ['-created_at']
 
 
 class Comments(models.Model):
